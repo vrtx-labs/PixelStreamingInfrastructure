3a9430931ff9fa494dd754905e994da54f4f944f
jbvrtx <jannik@vrtxlabs.com>
2023-08-09 12:18:21 +0200

    Match Viewport Resolution and use Hovering Mouse

--- SignallingWebServer/custom_html/vds.js
+++ SignallingWebServer/custom_html/vds.js
@@ -1,7 +1,6 @@
 // Constants
-const projectIDKey = 'ProjectID'
 const localStoragePrefix = 'velux.'
-
+const projectIDKey = 'ProjectID'
 const projectViewKey = 'ProjectView'
 const settingsProjectViewKey = 'SettingsProjectView'
 const roomOptionKey = 'RoomOption'
@@ -12,6 +11,7 @@ function setup() {
 	// Get URL parameter
 	parseURL();
 	setupPlayButton();
+	setTimeout(function(){ activateDefaultSettings(); }, 50);
 	//setupSlider ();
 }
 
@@ -28,14 +28,14 @@ function sendToStreamer(key, value) {
 		[key]: value
 	};
 	emitUIInteraction(descriptor);
-	console.log(`Message to streamer: ${key}: ${value}`)
+	console.log(`Message to streamer: ${descriptor}`)
 }
 
 function parseURL() {
 	// Read html attribute 'ProjectID'
     const parsedUrl = new URL(window.location.href)
     const projectID = parsedUrl.searchParams.get(projectIDKey) ? parsedUrl.searchParams.get(projectIDKey) : null
-	
+
 	console.log(`ProjectID: ${projectID}`)
 	if (projectID === null) return;
 	localStorageSet (projectIDKey, projectID);
@@ -46,6 +46,30 @@ function setupPlayButton (){
 	videoPlayOverlay.addEventListener('click', function onOverlayClick(event) {
 		startStream();
 	});
+}
+
+function activateDefaultSettings () {
+	// Set match viewport resolution to true
+	toggleRadioButton('match-viewport-res-tgl');
+
+	// Set the control scheme to Hovering Mouse
+	toggleRadioButton('control-tgl');
+}
+
+function toggleRadioButton (id) {
+	let radioButton = document.getElementById(id);
+	if (radioButton === null) return;
+
+	// Manually change the state of the button
+	radioButton.checked = !radioButton.checked;
+
+	// Create a new event and dispatch it
+	if ("createEvent" in document) {
+    var evt = document.createEvent("HTMLEvents");
+    evt.initEvent("change", false, true);
+    radioButton.dispatchEvent(evt);
+	}
+	else radioButton.onchange();
 }
 
 function localStorageSet (localStorageKey, localStorageValue)
@@ -68,6 +92,6 @@ function setupSlider () {
 	slider.oninput = function() {
 	  output.innerHTML = this.value;
 	  sendToStreamer(sliderKeyToStreamer, this.value);
-	} 
+	}
 }
 */
