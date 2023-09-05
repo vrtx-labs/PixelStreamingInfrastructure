96f868e6a806c5cdad3e77bbeb936596c7f3ba6c
Jannik Busse <jannik@vrtxlabs.com>
2023-09-05 15:28:59 +0200

    Include Hammer & JQuery to Make Joystick Sample Work

--- SignallingWebServer/custom_html/vds.js
+++ SignallingWebServer/custom_html/vds.js
@@ -8,7 +8,7 @@ const roomOptionKey = "roomOption";
 const daylightSliderKey = "daylightSliderValue";
 const screenshotKey = "screenshot";
 const mouseControlSchemeKey = "hoveringMouse";
-const domElements = {};
+const joystickValuesKey = "joystickValues";
 
 // Enumn
 class MenuContent {
@@ -27,7 +27,12 @@ class MenuContent {
 }
 
 // Variables
-let menuActive = false;
+const domElements = {}; // Holds references to DOM elements
+const GlobalVariables = {
+    menuActive: false,
+    joystickX: 0,
+    joystickY: 0,
+};
 
 // Setup: The event is linked to app.js OnLoadFinished in the setup function
 window.addEventListener("OnLoadFinished", () => {
@@ -48,6 +53,7 @@ function setupUIElements() {
     setupToggleMenuButton();
     setupRoomButtons();
     setupSlider();
+    setupJoystick();
     addResponseEventListener("handle_responses", receiveFromStreamer);
 }
 
@@ -116,13 +122,13 @@ function setupCopyLinkButton() {
 function toggleMenu() {
     let menu = domElements["menu"];
 
-    if (menuActive) {
-        menuActive = false;
+    if (GlobalVariables.menuActive) {
+        GlobalVariables.menuActive = false;
         // Toggle the menu by switching the class
         menu.classList.remove("menu-visible");
         menu.classList.add("menu-hidden");
     } else {
-        menuActive = true;
+        GlobalVariables.menuActive = true;
         // Toggle the menu by switching the class
         menu.classList.add("menu-visible");
         menu.classList.remove("menu-hidden");
@@ -221,6 +227,10 @@ function setupSlider() {
     updateSlider(slider.value);
 }
 
+function setupJoystick() {
+    initJoystick();
+}
+
 function activateDefaultSettings() {
     // Set match viewport resolution to true
     setRadioButtonState("match-viewport-res-tgl", true);
@@ -284,3 +294,96 @@ function localStorageGet(localStorageKey) {
     );
 }
 */
+
+/* ToDo: Import this from a separate file
+Copyright (c) 2023 by Jeff Treleaven (https://codepen.io/jiffy/pen/zrqwON)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+function initJoystick() {
+    // easal stuff goes hur
+    var xCenter = 150;
+    var yCenter = 150;
+    var stage = new createjs.Stage("joystick");
+
+    var psp = new createjs.Shape();
+    psp.graphics.beginFill("#333333").drawCircle(xCenter, yCenter, 50);
+
+    psp.alpha = 0.25;
+
+    var vertical = new createjs.Shape();
+    var horizontal = new createjs.Shape();
+    vertical.graphics.beginFill("#ff4d4d").drawRect(150, 0, 2, 300);
+    horizontal.graphics.beginFill("#ff4d4d").drawRect(0, 150, 300, 2);
+
+    stage.addChild(psp);
+    stage.addChild(vertical);
+    stage.addChild(horizontal);
+    createjs.Ticker.framerate = 60;
+    createjs.Ticker.addEventListener("tick", stage);
+    stage.update();
+
+    var joystick = document.getElementById("joystick");
+
+    // create a simple instance
+    // by default, it only adds horizontal recognizers
+    var mc = new Hammer(joystick);
+
+    mc.on("panstart", function (ev) {
+        var pos = joystick.offsetWidth - parseInt(joystick.style.width);
+        xCenter = psp.x;
+        yCenter = psp.y;
+        psp.alpha = 0.5;
+
+        stage.update();
+    });
+
+    // listen to events...
+    mc.on("panmove", function (ev) {
+        var pos = $("#joystick").position();
+
+        let x = ev.center.x - pos.left - 150;
+        let y = ev.center.y - pos.top - 150;
+
+        GlobalVariables.joystickX = Math.min(Math.max(x, -100), 100) / 100;
+        GlobalVariables.joystickY = Math.min(Math.max(y, -100), 100) / 100;
+
+        document.getElementById("xVal").innerText = "X: " + GlobalVariables.joystickX;
+        document.getElementById("yVal").innerText = "Y: " + GlobalVariables.joystickY;
+
+        var coords = calculateCoords(ev.angle, ev.distance);
+
+        psp.x = coords.x;
+        psp.y = coords.y;
+
+        psp.alpha = 0.5;
+
+        stage.update();
+    });
+
+    mc.on("panend", function (ev) {
+        GlobalVariables.joystickX = 0;
+        GlobalVariables.joystickY = 0;
+        document.getElementById("xVal").innerText = "X: " + GlobalVariables.joystickX;
+        document.getElementById("yVal").innerText = "Y: " + GlobalVariables.joystickY;
+
+        psp.alpha = 0.25;
+        createjs.Tween.get(psp).to({ x: xCenter, y: yCenter }, 750, createjs.Ease.elasticOut);
+    });
+}
+
+function calculateCoords(angle, distance) {
+    var coords = {};
+    distance = Math.min(distance, 100);
+    var rads = (angle * Math.PI) / 180.0;
+
+    coords.x = distance * Math.cos(rads);
+    coords.y = distance * Math.sin(rads);
+
+    return coords;
+}
