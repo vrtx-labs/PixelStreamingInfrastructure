377d7f19d2670e96bed45d8bc1d23bc2c917ffaa
Philipp Bontemps <philipp@vrtxlabs.com>
2023-12-08 00:04:27 +0100

    Update build files

--- SignallingWebServer/docker-build.bat
+++ SignallingWebServer/docker-build.bat
@@ -1,9 +1,12 @@
 @echo off
 :: Update your container registry name here
 SET CONTINAER_REG="velux.jfrog.io/docker-dev/ccoe/vds"
-:: Build Signalling Server Image
-docker "build" "-t" "signallingwebserver:5.1.1" "-f" "Dockerfile" .
-:: Tag and push images to container registry
-docker "tag" "signallingwebserver:5.1.1" "%CONTINAER_REG%/signallingwebserver:5.1.1"
+:: Update build version here
+SET BUILD_VERSION=5.1.4
 
-docker "push" "%CONTINAER_REG%/signallingwebserver:5.1.1"
+:: Build Signalling Server Image
+docker "build" "-t" "signallingwebserver:%BUILD_VERSION%" "-f" "Dockerfile" .
+:: Tag and push images to container registry
+docker "tag" "signallingwebserver:%BUILD_VERSION%" "%CONTINAER_REG%/signallingwebserver:%BUILD_VERSION%"
+
+docker "push" "%CONTINAER_REG%/signallingwebserver:%BUILD_VERSION%"
