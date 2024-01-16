ff99a6063b550a663ad4e2c6e1d53a93eb609014
Philipp Bontemps <philipp@vrtxlabs.com>
2024-01-17 00:13:22 +0100

    Add webfrontend
    Without VELUX customazations for now. Source code if from 4:27

---
+++ webfrontend/docker-build.bat
@@ -0,0 +1,16 @@
+@echo off
+:: Update your container registry name here
+SET CONTINAER_REG="velux.jfrog.io/docker-dev/ccoe/vds"
+
+:: Update your package name here
+SET PACKAGE_NAME="webfrontend"
+
+:: Update build version here
+SET BUILD_VERSION=0.1.0
+
+:: Build Unreal Engine App image . Copy the Dockerfile to the root of the Unreal Engine App
+docker "build" "-t" "%PACKAGE_NAME%:%BUILD_VERSION%" "-f" "Dockerfile" .
+
+:: Tag and push images to container registry
+docker "tag" "%PACKAGE_NAME%:%BUILD_VERSION%" "%CONTINAER_REG%/%PACKAGE_NAME%:%BUILD_VERSION%"
+docker "push" "%CONTINAER_REG%/%PACKAGE_NAME%:%BUILD_VERSION%"
